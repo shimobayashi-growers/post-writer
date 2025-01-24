9cadcd13317d511719233df9f702a34d1ee3f981
Takaaki Shimobayashi <takaaki.shimobayashi@growers.co.jp>
2025-01-24 15:17:19 +0900

    feat: loginのpage作成

---
+++ components/user-auth-form.tsx
@@ -0,0 +1,12 @@
+export default function UserAuthForm() {
+    return (
+        <div>
+            <form>
+                <div>
+                    <Label htmlFor="email">メールアドレス</Label>
+                    <Input id="email" type="email" />
+                </div>
+            </form>
+        </div>
+    )
+}
