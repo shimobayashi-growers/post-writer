2af9ede1557fe2576ccd6fdc583a4ded222396fb
Takaaki Shimobayashi <takaaki.shimobayashi@growers.co.jp>
2025-01-24 16:37:48 +0900

    chore: githubログインのsppiner追加

--- components/user-auth-form.tsx
+++ components/user-auth-form.tsx
@@ -6,9 +6,13 @@ import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import { buttonVariants } from "./ui/button";
 import { Icon } from "./icon";
+import { useState } from "react";
 
 
 export default function UserAuthForm() {
+
+    const [isGithubLoading, setIsGithubLoading] = useState<boolean>(false);
+
     return (
         <div className="grid gap-6">
             <form>
@@ -38,9 +42,12 @@ export default function UserAuthForm() {
 
             <button
                 className={cn(buttonVariants({ variant: "outline" }))}
-                onClick={() => signIn("github")}
+                onClick={() => {
+                    setIsGithubLoading(true);
+                    signIn("github")
+                }}
             >
-                <Icon.github className="mr-2"></Icon.github>
+                {isGithubLoading ? <Icon.spinner className="mr-2 animate-spin"></Icon.spinner> : <Icon.github className="mr-2"></Icon.github>}
                 Github
             </button>
         </div>
