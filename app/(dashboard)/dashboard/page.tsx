91a27a757be97db0d2a8d004b39e25777c972485
Takaaki Shimobayashi <takaaki.shimobayashi@growers.co.jp>
2025-01-31 22:04:05 +0900

    chore: ユーザのセッション状態を取得する関数を追加

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,8 +1,36 @@
 import DashBoardHeader from "@/components/dashboard-header";
 import DashBoardShell from "@/components/dashboard-shell";
 import PostCreateButton from "@/components/post-create-button";
+import { db } from "@/lib/db";
+import { getCurrentUser } from "@/lib/session";
+import { redirect } from "next/dist/server/api-utils";
 
-export default function DashBoardPage() {
+export default async function DashBoardPage() {
+
+    const user = await getCurrentUser();
+
+
+    if (!user) {
+        redirect("/login");
+    }
+
+    const posts = await db.post.findMany({
+        where: {
+            authorId: user.id,
+        },
+        select: {
+            id: true,
+            title: true,
+            published: true,
+            createdAt: true,
+        },
+        orderBy: {
+            updatedAt: "desc",
+        },
+    });
+
+    console.log(posts);
+
     return (
         <DashBoardShell>
             <DashBoardHeader heading="記事投稿" text="記事の投稿と管理">
