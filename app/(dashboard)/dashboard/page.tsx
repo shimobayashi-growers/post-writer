4430d33caa08c1fc9caebb7c3f99c233d99c520c
Takaaki Shimobayashi <takaaki.shimobayashi@growers.co.jp>
2025-01-31 21:41:17 +0900

    chore: DashBoardHeaderのcomponent追加

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,11 +1,14 @@
+import DashBoardHeader from "@/components/dashboard-header";
 import DashBoardShell from "@/components/dashboard-shell";
 
-export default function DashboardPage() {
+export default function DashBoardPage() {
     return (
         <DashBoardShell>
-            <div>
-                Dashboard
-            </div>
+            <DashBoardHeader heading="記事投稿" text="記事の投稿と管理">
+                <div>
+                    Dashboard
+                </div>
+            </DashBoardHeader>
         </DashBoardShell>
     );
 }
