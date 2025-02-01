ab736d3ae5d3da53a296eace7a7079717262fcc3
Takaaki Shimobayashi <takaaki.shimobayashi@growers.co.jp>
2025-02-01 21:31:15 +0900

    chore: 記事作成ボタンのAPI連携

--- components/post-create-button.tsx
+++ components/post-create-button.tsx
@@ -4,6 +4,7 @@ import { cn } from "@/lib/utils";
 import { ButtonProps, buttonVariants } from "./ui/button";
 import { useState } from "react";
 import { Icon } from "./icon";
+import { useRouter } from "next/navigation";
 
 interface PostCreateButtonProps extends ButtonProps {}
 
@@ -12,8 +13,34 @@ export default function PostCreateButton({
     variant,
     ...props
 }: PostCreateButtonProps) {
+    const router = useRouter();
     const [isLoading, setIsLoading] = useState(false);
-    const onClick = async () => {};
+    const onClick = async () => {
+        setIsLoading(true);
+
+        const response = await fetch("/api/posts", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                title: "Untitled"}),
+        });
+
+        setIsLoading(false);
+
+        console.log(response);
+
+        if (!response.ok) {
+            throw new Error("Failed to create post");
+        }
+
+        const post = await response.json();
+        router.refresh();
+        router.push(`/editor/${post.id}`);
+
+
+    };
 
     return (
         <button
